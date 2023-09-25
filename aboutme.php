d84f2d43d9c40d9a1d704113e27fbd8c7237f522
Prakhar Mishra <443prakharmishra@gmail.com>
2023-09-25 12:19:46 +0530

    added hostel

---
+++ aboutme.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+   <link rel="stylesheet" href="css\aboutme.css">
+   <title>Contact us</title>
+
+
+</head>
+<body>
+  <div class="heading" >
+      <h1>Our Team</h1>
+  </div>
+
+    <div class="container">
+
+        <div class="card">
+            <div class="content">
+                <div class="imgBx">
+                    <img src="images\bhupender.jpg" alt="">
+                </div>
+                <div class="contentBx">
+                    <h4>Bhupender</h4>
+                    <h5>Student</h5>
+                </div>
+                <div class="sci">
+                    <a href="https://m.facebook.com/profile.php?id=100037741577267"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/mohit_panwar_4127/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/BhupenderP54098"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <div class="imgBx">
+                    <img src="images\prakhar.jpg" alt="">
+                </div>
+                <div class="contentBx">
+                    <h4>Prakhar</h4>
+                    <h5>Student</h5>
+                </div>
+                <div class="sci">
+                    <a href="https://www.facebook.com/prakher.mishra.79"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/just_prakhar_things/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/Anonymo18426252"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
